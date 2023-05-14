e8719003963943370e1696bbd304a0d974966c0a
hemangini  ninama <77445208+ninama889@users.noreply.github.com>
2023-05-14 15:03:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,8 @@ try:
   else:
     back_from_function=get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
- except URLError as e:
+    
+except URLError as e:
  streamlit.error()
 
 #streamlit.stop()
