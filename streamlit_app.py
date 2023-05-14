2a01e5ec30e321bc2cc92ff00da6f564947ad2c8
hemangini  ninama <77445208+ninama889@users.noreply.github.com>
2023-05-14 14:45:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
- 
-
 streamlit.title('My Parents New Healthy Diner')
-
- 
 
 streamlit.header('Breckfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -17,28 +13,19 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
- 
 
 #import pandas
 
- 
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
- 
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']) default selection
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
- 
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
- 
 
 #Create repeatable code block(a function)
 def get_fruityvice_data(this_fruit_choice):
@@ -60,8 +47,6 @@ except URLError as e:
 
 #streamlit.stop()
 
- 
-
 streamlit.text("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
@@ -75,8 +60,6 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows =get_fruit_load_list()
   my_cnx.close()
   streamlit.dataframe(my_data_rows)
-
- 
 
 # Allow end user to add fruit to the list
 def insert_row_snowflake(new_fruit):
